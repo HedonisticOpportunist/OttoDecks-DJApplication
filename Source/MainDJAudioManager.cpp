1b2757a37eb037b2382684d4ccdfea181b1d857d
HedonisticOpportunist <fujitahibaraki@gmail.com>
2022-02-16 23:26:57 +0000

    Add files via upload

--- Source/MainDJAudioManager.cpp
+++ Source/MainDJAudioManager.cpp
@@ -14,8 +14,8 @@ MainDJAudioManager::MainDJAudioManager()
     setSize(1200, 800);
 
     // add background image 
-    // credit @ https://unsplash.com/@markusspiske
-    backgroundImage = ImageCache::getFromMemory(BinaryData::background_png, BinaryData::background_pngSize);
+    // https://c4.wallpaperflare.com/wallpaper/548/896/920/cat-headphones-lilac-funny-wallpaper-preview.jpg
+    backgroundImage = ImageCache::getFromMemory(BinaryData::cat_background_png, BinaryData::cat_background_pngSize);
 
     if (juce::RuntimePermissions::isRequired(juce::RuntimePermissions::recordAudio)
         && !juce::RuntimePermissions::isGranted(juce::RuntimePermissions::recordAudio))
@@ -87,15 +87,15 @@ void MainDJAudioManager::resized()
     **/
 
     // DECKS
-    deck1.setBounds(0, 0, getWidth() / 3.2, getHeight() / 3.2);
-    deck2.setBounds(getWidth() / 3.2, 0, getWidth() / 3.2, getHeight() / 3.2);
+    deck1.setBounds(0, 0, getWidth() / 4.2, getHeight() / 4.2);
+    deck2.setBounds(getWidth() / 4.2, 0, getWidth() / 4.2, getHeight() / 4.2);
 
     // MUSIC CONTROL DECK 
-    musicControlDeck.setBounds(0, getHeight() / 3.2, getWidth() / 3.2, getHeight() / 3.2);
+    musicControlDeck.setBounds(0, getHeight() / 4.2, getWidth() / 4.2, getHeight() / 4.2);
 
     // PLAYLIST
-    playListComponent.setBounds(getWidth() / 3.2, getHeight() / 3.2, getWidth() / 3.2, getHeight() / 3.2);
+    playListComponent.setBounds(getWidth() / 4.2, getHeight() / 4.2, getWidth() / 4.2, getHeight() / 4.2);
 
     // SEARCH FIELD 
-    searchField.setBounds(0, getHeight() - 300, getWidth() / 4.2, getHeight() / 4.2);
+    searchField.setBounds(0, getHeight() - 420, getWidth() / 4.2, getHeight() / 4.2);
 }
